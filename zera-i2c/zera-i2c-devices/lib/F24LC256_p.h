54171a900d06c669dc6e10ad8263f031f136db3c
Andreas Müller <schnitzeltony@gmail.com>
2025-05-12 17:55:47 +0200

    cF24LC256Private: Remove unneeded dtor / make methods private
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-i2c/zera-i2c-devices/lib/F24LC256_p.h
+++ zera-i2c/zera-i2c-devices/lib/F24LC256_p.h
@@ -8,15 +8,15 @@ class cF24LC256Private: public I2cFlashInterface
 {
 public:
     cF24LC256Private(QString devNode, short adr);
-    virtual ~cF24LC256Private() = default;
     int WriteData(char* data, ushort count, ushort memAddress) override;
     int ReadData(char* data, ushort count, ushort memAddress) override;
     int Reset() override;
     int size() override;
 
+private:
     QString getDeviceNodeName();
     ushort getI2cAddress();
-protected:
+
     QString m_devNodeName;
     ushort m_i2cAdress;
 };
